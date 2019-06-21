086b904d92c311cc2e5600f8cf8a789468e22492
ebisLab <ebaiden@techie.com>
2019-06-21 09:22:30 -0400

    removed active-tab

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -31,30 +31,31 @@ class TabLink {
     
    //Add a click event that invokes this.selectTab
     this.tabElement.addEventListener('click', ()=>this.selectTab());
-    console.log('Suprise MF*KA!!')
+    
     
   }
 
-  // selectTab(){
-
-  //   // Select all elements with the .tab class on them
-  //   // const tabs = document.querySelectorAll();
+  selectTab(){
+    console.log('Suprise MF*KA!!')
+    // Select all elements with the .tab class on them
+    const tabs = document.querySelectorAll('.all');
     
-  //   // Iterate through the NodeList removing the .active-tab class from each element
-  //   // tabs.forEach()
-
-  //   // Select all of the elements with the .card class on them
-  //   // const cards = ;
-
-  //   // Iterate through the NodeList setting the display style each one to 'none'
-  //   // cards.forEach()
+    // Iterate through the NodeList removing the .active-tab class from each element
+    tabs.forEach(element => element.classList.remove('.active-tab'))
     
-  //   // Add a class of ".active-tab" to this.tabElement
-  //   // this.tabElement;
+
+    // Select all of the elements with the .card class on them
+    // const cards = ;
+
+    // Iterate through the NodeList setting the display style each one to 'none'
+    // cards.forEach()
+    
+    // Add a class of ".active-tab" to this.tabElement
+    // this.tabElement;
   
-  //   // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-  //   // this.cards.forEach(card => card.selectCard());
-  // }
+    // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
+    // this.cards.forEach(card => card.selectCard());
+  }
 }
 
 class TabCard {
